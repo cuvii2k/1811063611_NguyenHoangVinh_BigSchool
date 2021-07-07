139e0082466287847efae8b980bc29a31fc2651a
NguyenVinh <95vinhndu1415@gmail.com>
2021-07-07 15:02:12 +0700

    Update 3h01 7-7-2021 page: 49 Errors: index.cshtml

--- 1811063611_NguyenHoangVinh_BigSchool/Controllers/HomeController.cs
+++ 1811063611_NguyenHoangVinh_BigSchool/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using _1811063611_NguyenHoangVinh_BigSchool.Models;
 using System.Data.Entity;
+using _1811063611_NguyenHoangVinh_BigSchool.ViewModels;
 
 namespace _1811063611_NguyenHoangVinh_BigSchool.Controllers
 {
@@ -16,7 +17,13 @@ namespace _1811063611_NguyenHoangVinh_BigSchool.Controllers
                 .Include(c => c.Lecturer)
                 .Include(c => c.Category)
                 .Where(c=>c.DateTime > DateTime.Now);
-            return View(upcommingCourses);
+            //return View(upcommingCourses);
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ 1811063611_NguyenHoangVinh_BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _1811063611_NguyenHoangVinh_BigSchool.Models;
+
+namespace _1811063611_NguyenHoangVinh_BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+
+    }
+}
